14399b0fb7d5eee8912b6ea9eb2e1035d8ccdf79
Maylee <mgagnon987@gmail.com>
2019-11-24 16:30:32 -0500

    working!?

--- gaussjordan/src/gaussjordan.cpp
+++ gaussjordan/src/gaussjordan.cpp
@@ -6,11 +6,11 @@
 #include <cmath>
 using namespace std;
 
-#define n 3
+#define n 8
 
 /*Prints the given array
  */
-void printArr (int A[][n+1]){
+void printArr (double A[][n+1]){
 	for (int i = 0; i < n; i++){
 		for (int j = 0; j <= n; j++){
 			cout << A[i][j] << ", ";
@@ -63,7 +63,7 @@ int betterForwardElimin(int A[][4], int b[3]){
  * @param b
  * @return
  */
-int gaussJordanAlg(int A[][n+1], int b[n]){
+int gaussJordanAlg(double A[][n+1], double b[n]){
 	for (int i = 0; i < n; i++){
 		A[i][n] = b[i]; // appends b to A as the last col
 	}
@@ -78,23 +78,20 @@ int gaussJordanAlg(int A[][n+1], int b[n]){
 		}
 
 		for (int k = i; k < n+1; k++){ // swapping rows
-			int tempEntry = A[i][k];
+			double tempEntry = A[i][k];
 			A[i][k] = A[pivotrow][k];
 			A[pivotrow][k] = tempEntry;
 		}
 
-		printArr(A);
-		cout << endl;
-
 		// GJ modification, each pivot row is divided by leading entry prior to use as pivot row, producing an identity matrix
-		float leadingEntry = A[i][i];
+		double leadingEntry = A[i][i];
 		for (int x = i; x < n+1; x++) {
 			A[i][x] = A[i][x] / leadingEntry;
 		}
 
-		for (int j = 0; j < n; j++){ //zeroing/updating values
+		for (int j = 0; j < n; j++){ //zeroing/updating values, does upper & lower
 			if (i!=j){
-				float temp = (float)A[j][i] / (float)A[i][i];
+				double temp = (double)A[j][i] / (double)A[i][i];
 				for (int k = 0; k < n+1; k++){
 					A[j][k] = A[j][k] - (A[i][k]*temp);
 				}
@@ -106,11 +103,8 @@ int gaussJordanAlg(int A[][n+1], int b[n]){
 	return 0;
 }
 
-
 int main() {
-
-
-	int arrHW[8][9] = {{1, 1, 1, 1, 1, 1, 1, 1, 0},
+	double arrHW[8][9] = {{1, 1, 1, 1, 1, 1, 1, 1, 0},
 					  {1, 2, 1, 1, 1, 1, 2, 1, 0},
 					  {1, 1, 3, 1, 1, 3, 1, 1, 0},
 					  {1, 1, 1, 4, 4, 1, 1, 1, 0},
@@ -118,7 +112,7 @@ int main() {
 					  {1, 1, 1, 1, -1, -1, -1, -1, 0},
 					  {1, 2, 3, 4, 5, 6, 7, 8, 0},
 					  {1, -1, 1, -1, 1, -1, 1, -1, 0}};
-	int bHW[8] = {0, 0, 0, 0, 20, 34, -51, -6};
+	double bHW[8] = {0, 0, 0, 0, 20, 34, -51, -6};
 	cout << "gaussJordan. Original array" << endl;
 	printArr(arrHW);
 	cout << endl;
@@ -128,7 +122,6 @@ int main() {
 	cout << "array after" << endl;
 	printArr(arrHW);
 	cout << endl;
-
 
 	/*
 	int arrEx[3][4] = {{2, -1, 1, 0},
@@ -146,11 +139,11 @@ int main() {
 	printArr(arrEx);
 	 */
 
-/*
-	int arrExGJ[3][4] = {{2, -1, 1, 0},
+	/*
+	double arrExGJ[3][4] = {{2, -1, 1, 0},
 			{4, 1, -1, 0},
 			{1, 1, 1, 0}};
-	int bExGJ[3] = {1, 5, 0};
+	double bExGJ[3] = {1, 5, 0};
 	cout << "gaussJordan. Original array" << endl;
 	printArr(arrExGJ);
 	cout << endl;
@@ -160,6 +153,7 @@ int main() {
 	cout << "array after" << endl;
 	printArr(arrExGJ);
 	cout << endl;
-*/
+	 */
+
 	return 0;
 }
