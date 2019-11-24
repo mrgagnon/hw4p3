48f5bf551641f8ece193e0250c72c961dc3d1955
Maylee <mgagnon987@gmail.com>
2019-11-24 16:09:30 -0500

    some kind of result. 10 not 20 though

--- gaussjordan/src/gaussjordan.cpp
+++ gaussjordan/src/gaussjordan.cpp
@@ -10,20 +10,22 @@ using namespace std;
 
 /*Prints the given array
  */
-void printArr (int A[][4]){
-	for (int i = 0; i < 3; i++){
-		for (int j = 0; j < 4; j++){
+void printArr (int A[][n+1]){
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j <= n; j++){
 			cout << A[i][j] << ", ";
 		}
 		cout << endl;
 	}
 }
+
 /* Uses the betterFowardElimination algorithm from the textbook so solve the system of equations
  * @param A
  * @param b
  * @return
 
  */
+/*
 int betterForwardElimin(int A[][4], int b[3]){
 	for (int i = 0; i < n; i++){
 		A[i][n] = b[i]; // appends b to A as the last col
@@ -45,8 +47,6 @@ int betterForwardElimin(int A[][4], int b[3]){
 		}
 
 		for (int j = i+1; j < n; j++){ //zeroing/updating values
-			cout << "in for loop" << endl;
-			cout << "j,i " << A[j][i] << " i,i " << A[i][i] << endl;
 			float temp = (float)A[j][i] / (float)A[i][i];
 			cout << temp << endl;
 			for (int k = i; k < n+1; k++){
@@ -56,19 +56,19 @@ int betterForwardElimin(int A[][4], int b[3]){
 	}
 
 	return 0;
-}
+}*/
 
 /* Modifies the betterforward algorithm to follow the gaussJordan algorithm
  * @param A
  * @param b
  * @return
  */
-int gaussJordanAlg(int A[][4], int b[3]){
+int gaussJordanAlg(int A[][n+1], int b[n]){
 	for (int i = 0; i < n; i++){
 		A[i][n] = b[i]; // appends b to A as the last col
 	}
 
-	for (int i = 0; i < n-1; i++){
+	for (int i = 0; i < n; i++){ //TODO n-1?
 		int pivotrow = i;
 
 		for (int j = i+1; j < n; j++){ //finding max row
@@ -83,40 +83,57 @@ int gaussJordanAlg(int A[][4], int b[3]){
 			A[pivotrow][k] = tempEntry;
 		}
 
-		for (int j = i+1; j < n; j++){ //zeroing/updating values
-			cout << "in for loop" << endl;
-			cout << "j,i " << A[j][i] << " i,i " << A[i][i] << endl;
-			float temp = (float)A[j][i] / (float)A[i][i];
-			cout << temp << endl;
-			for (int k = i; k < n+1; k++){
-				A[j][k] = A[j][k] - (A[i][k]*temp);
+		printArr(A);
+		cout << endl;
+
+		// GJ modification, each pivot row is divided by leading entry prior to use as pivot row, producing an identity matrix
+		float leadingEntry = A[i][i];
+		for (int x = i; x < n+1; x++) {
+			A[i][x] = A[i][x] / leadingEntry;
+		}
+
+		for (int j = 0; j < n; j++){ //zeroing/updating values
+			if (i!=j){
+				float temp = (float)A[j][i] / (float)A[i][i];
+				for (int k = 0; k < n+1; k++){
+					A[j][k] = A[j][k] - (A[i][k]*temp);
+				}
 			}
 		}
+		printArr(A);
+		cout << endl;
 	}
-
 	return 0;
 }
 
 
 int main() {
-	cout << "Hello" << endl;
+
+
+	int arrHW[8][9] = {{1, 1, 1, 1, 1, 1, 1, 1, 0},
+					  {1, 2, 1, 1, 1, 1, 2, 1, 0},
+					  {1, 1, 3, 1, 1, 3, 1, 1, 0},
+					  {1, 1, 1, 4, 4, 1, 1, 1, 0},
+					  {11, 1, 1, 1, 1, 1, 1, 1, 0},
+					  {1, 1, 1, 1, -1, -1, -1, -1, 0},
+					  {1, 2, 3, 4, 5, 6, 7, 8, 0},
+					  {1, -1, 1, -1, 1, -1, 1, -1, 0}};
+	int bHW[8] = {0, 0, 0, 0, 20, 34, -51, -6};
+	cout << "gaussJordan. Original array" << endl;
+	printArr(arrHW);
+	cout << endl;
+
+	gaussJordanAlg(arrHW, bHW);
+
+	cout << "array after" << endl;
+	printArr(arrHW);
+	cout << endl;
+
 
 	/*
-	int arrHW[8][9] = {{1, 1, 1, 1, 1, 1, 1, 1, 0},
-					 {1, 2, 1, 1, 1, 1, 2, 1, 0},
-					 {1, 1, 3, 1, 1, 3, 1, 1, 0},
-					 {1, 1, 1, 4, 4, 1, 1, 1, 0},
-					 {11, 1, 1, 1, 1, 1, 1, 1, 0},
-					 {1, 1, 1, 1, 1, 1, 1, 1, 0},
-					 {1, 2, 3, 4, 5, 6, 7, 8, 0},
-					 {1, 1, 1, 1, 1, 1, 1, 1, 0}};
-	int bHW[8] = {0, 0, 0, 0, 20, 34, 51, 6};
-	*/
-
-
 	int arrEx[3][4] = {{2, -1, 1, 0},
-					   {4, 1, -1, 0},
-					   {1, 1, 1, 0}};
+			{4, 1, -1, 0},
+			{1, 1, 1, 0}};
 	int bEx[3] = {1, 5, 0};
 
 	cout << "original array" << endl;
@@ -127,7 +144,22 @@ int main() {
 
 	cout << "array after" << endl;
 	printArr(arrEx);
-	cout << "Hi" << endl;
+	 */
 
+/*
+	int arrExGJ[3][4] = {{2, -1, 1, 0},
+			{4, 1, -1, 0},
+			{1, 1, 1, 0}};
+	int bExGJ[3] = {1, 5, 0};
+	cout << "gaussJordan. Original array" << endl;
+	printArr(arrExGJ);
+	cout << endl;
+
+	gaussJordanAlg(arrExGJ, bExGJ);
+
+	cout << "array after" << endl;
+	printArr(arrExGJ);
+	cout << endl;
+*/
 	return 0;
 }
